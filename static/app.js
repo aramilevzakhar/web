a69f6c51fa590a2f2556f534ffc95ae20ac97f26
z9one <ar.z.v@yandex.ru>
2022-04-26 23:12:16 +0500

    up

--- static/app.js
+++ static/app.js
@@ -29,36 +29,35 @@ app.get("/download", (req, res) => {
 	res.download(path.resolve(__dirname, '1.mp4'));
 });
 
-
 app.get("/video", (req, res) => {
-	const range = req.headers.range;
-	console.log(range)
-	if (!range) {
-		res.status(400).send("Requires Range header");
-	}
+		const range = req.headers.range;
+		console.log(range);
+		if (!range) {
+			res.status(400).send("Requires Range header");
+		}
 
 
-	console.log('movie_name: ', movie_name)
-	const videoPath = "static/1.mp4";
-	console.log(videoPath)
-	const videoSize = fs.statSync(videoPath).size;
-	const CHUNK_SIZE = 10 ** 6;
-	const start = Number(range.replace(/\D/g, ""))
-	const end = Math.min(start + CHUNK_SIZE, videoSize - 1)
-	const contentLength = end - start + 1
+		console.log('movie_name: ', movie_name);
+		const videoPath = "static/1.mp4";
+		console.log(videoPath);
+		const videoSize = fs.statSync(videoPath).size;
+		const CHUNK_SIZE = 10 ** 6;
+		const start = Number(range.replace(/\D/g, ""));
+		const end = Math.min(start + CHUNK_SIZE, videoSize - 1);
+		const contentLength = end - start + 1;
 
-	const headers = {
-		"Content-Range": `bytes ${start}-${end}/${videoSize}`,
-		"Accept-Ranges": "bytes",
-		"Content-Length": contentLength,
-		"Content-Type": "video/mp4",
-	}
+		const headers = {
+			"Content-Range": `bytes ${start}-${end}/${videoSize}`,
+			"Accept-Ranges": "bytes",
+			"Content-Length": contentLength,
+			"Content-Type": "video/mp4",
+		};
 
-	res.writeHead(206, headers);
-	const videoStream = fs.createReadStream(videoPath, {start, end});
-	videoStream.pipe(res)
+		res.writeHead(206, headers);
+		const videoStream = fs.createReadStream(videoPath, { start, end });
+		videoStream.pipe(res);
 
-});
+	});
 
 app.use(bodyParser.json());
 app.post("/save", async (request, response) => {
